e7336b203b44b0e410f51b464910494113a1cb2c
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-03-22 21:47:25 +0300

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Fabfile to generates a .tgz archive from the contents of web_static.
+# Fabfile to generates.tgz archive from the contents of web_static.
 import os.path
 from datetime import datetime
 from fabric.api import local
