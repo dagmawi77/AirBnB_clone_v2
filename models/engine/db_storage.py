08bf283eef7bd88aee5b626bf07413bc1e1804d1
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-03-22 22:41:30 +0300

    engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Defines the DBStorage engine."""
 from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -10,9 +9,7 @@ from models.review import Review
 from models.state import State
 from models.user import User
 from sqlalchemy import create_engine
-from sqlalchemy.orm import relationship
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 class DBStorage:
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,57 +1,55 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
+""" Defines FileStorage class. """
 import json
 from models.base_model import BaseModel
-from models.amenity import Amenity
+from models.user import User
+from models.state import State
 from models.city import City
+from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from models.state import State
-from models.user import User
 
 
 class FileStorage:
-    """Represent an abstracted storage engine.
+    """
+    Serializes instances to a JSON file and deserializes
+    JSON file to instances.
 
     Attributes:
-        __file_path (str): The name of the file to save objects to.
-        __objects (dict): A dictionary of instantiated objects.
+        __file_path (str): Path to the JSON file.
+        __objects (dict): Store all objects.
     """
-
     __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
-        """Return a dictionary of instantiated objects in __objects.
-
-        If a cls is specified, returns a dictionary of objects of that type.
-        Otherwise, returns the __objects dictionary.
-        """
+        """ Returns the dictionary __objects. """
         if cls is not None:
             if type(cls) == str:
                 cls = eval(cls)
             cls_dict = {}
-            for k, v in self.__objects.items():
-                if type(v) == cls:
-                    cls_dict[k] = v
+            for key, value in self.__objects.items():
+                if type(value) == cls:
+                    cls_dict[key] = value
             return cls_dict
+
         return self.__objects
 
     def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id."""
+        """ Sets in __objects the obj with key<obj class name>.id."""
         self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
 
     def save(self):
-        """Serialize __objects to the JSON file __file_path."""
+        """ Serializes __objects to the JSON file."""
         odict = {o: self.__objects[o].to_dict() for o in self.__objects.keys()}
-        with open(self.__file_path, "w", encoding="utf-8") as f:
-            json.dump(odict, f)
+        with open(self.__file_path, "w", encoding="utf-8") as myfile:
+            json.dump(odict, myfile)
 
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-            with open(self.__file_path, "r", encoding="utf-8") as f:
-                for o in json.load(f).values():
+            with open(self.__file_path, "r", encoding="utf-8") as myfile:
+                for o in json.load(myfile).values():
                     name = o["__class__"]
                     del o["__class__"]
                     self.new(eval(name)(**o))
@@ -59,7 +57,9 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """Delete a given object from __objects, if it exists."""
+        """Delete obj from __objects if it’s inside - if obj is equal to None,
+        The method should not do anything
+        """
         try:
             del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
         except (AttributeError, KeyError):
