5634a9e67e0eadf3cd07785c728af027960a1b1c
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-03-22 22:27:46 +0300

    setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,19 +1,17 @@
--- Creates a MySQL server with:
---   Database hbnb_dev_db.
---   User hbnb_dev with password hbnb_dev_pwd in localhost.
---   Grants all privileges for hbnb_dev on hbnb_dev_db.
---   Grants SELECT privilege for hbnb_dev.
+-- Script that prepares a MySQL server for the project.
+-- A database hbnb_dev_db
+-- A new use hbnb_dev in localhost.
+-- The password hbnb_dev_pwd.
+-- The database hbnb_dev_db
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
+    IF NOT EXISTS `hbnb_dev`@`localhost`
     IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES
-   ON 'hbnb_dev_db'.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+   ON `hbnb_dev_db`.*
+   TO `hbnb_dev`@`localhost`;
 GRANT SELECT
-   ON 'performance_schema'.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+   ON `performance_schema`.*
+   TO `hbnb_dev`@`localhost`;
 FLUSH PRIVILEGES;
