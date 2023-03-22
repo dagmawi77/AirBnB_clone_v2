109d5212e944c96da40da52ccfe19b188f1b5bb3
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-03-22 21:51:45 +0300

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -29,12 +29,6 @@ def do_pack():
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
-
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
     """
     if os.path.isfile(archive_path) is False:
         return False
