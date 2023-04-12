0b4b58c06de9f156bdc7365d46f423310092a7a0
dagmawi77 <dagmawiletarik@gmail.com>
2023-04-12 00:53:51 -0700

    Correct the Scripts

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,43 +1,41 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+"""
+Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers, using the function do_deploy
+"""
+from os import path
+from fabric.api import env, put, run
 
 env.hosts = ["52.91.132.17", "34.224.63.71"]
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
     """
-    if os.path.isfile(archive_path) is False:
+    Distributes archives to web servers
+    """
+    if not path.exists(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
-
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    compressedFile = archive_path.split("/")[-1]
+    fileName = compressedFile.split(".")[0]
+    upload_path = "/tmp/{}".format(compressedFile)
+    if put(archive_path, upload_path).failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+    current_release = '/data/web_static/releases/{}'.format(fileName)
+    if run("sudo rm -rf {}".format(current_release)).failed:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
+    if run("sudo mkdir -p {}".format(current_release)).failed:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
+    uncompress = "sudo tar -xzf /tmp/{} -C {}".format(
+        compressedFile, current_release
+    )
+    if run(uncompress).failed:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+    delete_archive = "sudo rm -f /tmp/{}".format(compressedFile)
+    if run(delete_archive).failed:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+    if run("sudo rm -rf /data/web_static/current").failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
+    relink = "sudo ln -s {} /data/web_static/current".format(current_release)
+    if run(relink).failed:
         return False
     return True
