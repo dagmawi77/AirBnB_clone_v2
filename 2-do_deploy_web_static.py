4645c31de010fe16a601f5b1530e4b28a00d21f0
dagmawi77 <dagmawiletarik@gmail.com>
2023-04-12 01:16:01 -0700

    edit script 3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,41 +1,36 @@
 #!/usr/bin/python3
 """
-Fabric script (based on the file 1-pack_web_static.py)
-that distributes an archive to your web servers, using the function do_deploy
+    deploy static
 """
-from os import path
-from fabric.api import env, put, run
-
-env.hosts = ["52.91.132.17", "34.224.63.71"]
+# import time
+# from fabric.context_managers import cd
+from fabric.api import local
+from fabric.api import get
+from fabric.api import put
+from fabric.api import reboot
+from fabric.api import run
+from fabric.api import sudo
+from fabric.api import env
+env.hosts = ['52.91.132.17', '34.224.63.71']
 
 
 def do_deploy(archive_path):
-    """
-    Distributes archives to web servers
-    """
-    if not path.exists(archive_path):
+    """ deploy my archive tgz into my servers """
+    try:
+        put(archive_path, '/tmp/')
+        c1 = 'sudo mkdir -p /data/web_static/releases/{}/'
+        run(c1.format(archive_path[9:-4]))
+        c2 = 'sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+        run(c2.format(archive_path[9:], archive_path[9:-4]))
+        run('sudo rm /tmp/{}'.format(archive_path[9:]))
+        c3 = 'mv /data/web_static/releases/{}/web_static/* \
+              /data/web_static/releases/{}/'
+        run(c3.format(archive_path[9:-4], archive_path[9:-4]))
+        c4 = 'sudo rm -rf  /data/web_static/releases/{}/web_static/'
+        run(c4.format(archive_path[9:-4]))
+        run('sudo rm -rf /data/web_static/current')
+        c5 = 'sudo ln -s /data/web_static/releases/{}/ {}'
+        run(c5.format(archive_path[9:-4], '/data/web_static/current'))
+        return True
+    except:
         return False
-    compressedFile = archive_path.split("/")[-1]
-    fileName = compressedFile.split(".")[0]
-    upload_path = "/tmp/{}".format(compressedFile)
-    if put(archive_path, upload_path).failed:
-        return False
-    current_release = '/data/web_static/releases/{}'.format(fileName)
-    if run("sudo rm -rf {}".format(current_release)).failed:
-        return False
-    if run("sudo mkdir -p {}".format(current_release)).failed:
-        return False
-    uncompress = "sudo tar -xzf /tmp/{} -C {}".format(
-        compressedFile, current_release
-    )
-    if run(uncompress).failed:
-        return False
-    delete_archive = "sudo rm -f /tmp/{}".format(compressedFile)
-    if run(delete_archive).failed:
-        return False
-    if run("sudo rm -rf /data/web_static/current").failed:
-        return False
-    relink = "sudo ln -s {} /data/web_static/current".format(current_release)
-    if run(relink).failed:
-        return False
-    return True
