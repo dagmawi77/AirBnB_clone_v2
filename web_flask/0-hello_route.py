3632003e071505d4182526f8250ef338e17fa473
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-04-23 15:13:17 +0300

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
+"""The application listens on 0.0.0.0, port 5000.
 /: Displays 'Hello HBNB!'
 """
 from flask import Flask
